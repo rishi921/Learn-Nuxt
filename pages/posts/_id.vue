b70e9cb0e84a87aab093a9143e8c211a67f05378
rishi921 <shrishidurga21@gmail.com>
2024-08-28 13:00:48 +0530

    Updated

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <h1>{{ post.body }}</h1>
+    <nuxt-link to="/posts">Back To Posts</nuxt-link>
+  </div>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            post: {}
+        }
+    },
+    async fetch (){
+        const post = await this.$axios.$get(`https://jsonplaceholder.typicode.com/posts/${this.$route.$params.id}`)
+    this.post = post
+    }
+};
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Blog Posts</h1>
-    <PostCard v-for="post in posts" :key="post.id" :post="post" />
+    <!-- <PostCard v-for="post in posts" :key="post.id" :post="post" /> -->
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <nuxt-link :to="`/posts/${post.id}`">{{ post.title }}</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import PostCard from "../../components/PostCard.vue";
+import PostCard from "~/components/PostCard.vue";
+
 export default {
   components: {
     PostCard,
@@ -15,7 +21,7 @@ export default {
     const posts = await $axios.$get(
       "https://jsonplaceholder.typicode.com/posts"
     );
-    return posts;
+    return { posts };
   },
 };
 </script>
